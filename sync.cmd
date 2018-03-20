9c70c3331b4b170bb2733863bace859638f471ee
Hans_Huang <713han@gmail.com>
2018-03-20 14:22:06 +0800

    project init

---
+++ sync.cmd
@@ -0,0 +1,25 @@
+SETLOCAL
+set log_time=%date:~0,4%-%date:~5,2%-%date:~8,2%
+set log_path=G:\Log\Sync
+
+IF NOT EXIST %log_path% (
+	mkdir %log_path%
+)
+
+CALL :sync D:\, F:\disk_D, %log_path%\%log_time%_D.log
+CALL :sync E:\, F:\disk_E, %log_path%\%log_time%_E.log
+
+EXIT /B %ERRORLEVEL%
+
+:sync
+IF EXIST %~3 (
+	"C:\Program Files (x86)\Windows Resource Kits\Tools\robocopy.exe" %~1 %~2 /mir /xa:sh /w:3 /r:1 /v /np /log+:%~3
+) ELSE (
+ 	"C:\Program Files (x86)\Windows Resource Kits\Tools\robocopy.exe" %~1 %~2 /mir /xa:sh /w:3 /r:1 /v /np /log:%~3
+)
+
+attrib -s -h %~2
+EXIT /B 0
+
+
+
